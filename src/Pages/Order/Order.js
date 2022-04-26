e5ecc5b3f422d8604f671b1ec522ae8e0f43dc85
Syed Monirul Alam <Rambling.ctg@gmail.com>
2022-04-27 02:25:01 +0600

    added axios private and 401 403 error handle and added jwt

--- src/Pages/Order/Order.js
+++ src/Pages/Order/Order.js
@@ -1,20 +1,36 @@
-import axios from 'axios';
+import { signOut } from 'firebase/auth';
 import React, { useEffect, useState } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import { useNavigate } from 'react-router-dom';
+import axiosPrivate from '../../api/axios.private';
 import auth from '../../firebase.init';
 
 const Order = () => {
   const [user] = useAuthState(auth);
   const [orders, setOrders] = useState([]);
+  const navigate = useNavigate();
   useEffect(() => {
     const getOrders = async () => {
-      const email = user.email;
+      const email = user?.email;
       const url = `http://localhost:5000/orders?email=${email}`;
-      const { data } = await axios.get(url);
-      setOrders(data);
+      try {
+        const { data } = await axiosPrivate.get(url);
+        // const { data } = await axios.get(url, {
+        //   headers: {
+        //     authorization: `Bearer ${localStorage.getItem('accessToken')}`,
+        //   },
+        // });
+        setOrders(data);
+      } catch (error) {
+        console.log(error);
+        if (error.response.status === 401 || error.response.status === 403) {
+          signOut(auth);
+          navigate('/login');
+        }
+      }
     };
     getOrders();
-  }, []);
+  }, [user?.email]);
   return (
     <div>
       <h3>Your Orders {orders.length} </h3>
